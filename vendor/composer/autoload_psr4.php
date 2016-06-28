3c8095ee6f62255908a598833b6b2c5e87cab853
José Jaraba <jijaraba@hotmail.com>
2016-06-27 20:39:35 -0500

    Composer Update

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'SigeTurbo\\SMS\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3509bae1b43f46f35451983aa30aec82
 {
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'SigeTurbo\\SMS\\' => 14,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'SigeTurbo\\SMS\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3509bae1b43f46f35451983aa30aec82::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3509bae1b43f46f35451983aa30aec82::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
